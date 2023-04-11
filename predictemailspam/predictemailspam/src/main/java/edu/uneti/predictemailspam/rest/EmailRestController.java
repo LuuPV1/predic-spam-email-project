ac58a3342512b6a8e7ef2c8d10141e33f8551455
LuuPV1 <phamluu.cntt@gmail.com>
2023-04-12 00:31:40 +0700

    add Mail Rest controller

---
+++ predictemailspam/predictemailspam/src/main/java/edu/uneti/predictemailspam/rest/EmailRestController.java
@@ -0,0 +1,12 @@
+package edu.uneti.predictemailspam.rest;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/v1/email")
+public class EmailRestController {
+
+}
